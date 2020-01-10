51294601873081f5e5a8b2755c33f80c2c778d9a
benspector3 <ben@operationspark.org>
2020-01-10 16:12:48 -0600

    template code for program with timer interval

---
+++ README.md
@@ -0,0 +1,30 @@
+Note: These project instructions are written for Mac OSX
+
+# Game
+
+**Table of Contents**
+- [Installation & Setup](#installation--setup)
+
+# Installation & Setup
+
+## To download this project into your workspace:
+
+First locate the folder in which you would like to download this code. If you need a lesson on how to navigate your file system using the Terminal, see the link below:
+
+https://www.macworld.com/article/2042378/master-the-command-line-navigating-files-and-folders.html
+
+Use the `cd` command to navigate to the folder you wish to use. Then:
+
+1) Fork it (Click the "Fork" button in the top right corner of the github repository for this project and fork it to your
+account
+2) Click on the "Clone or Download" button and copy the github URL
+3) Enter this command into your bash terminal:
+
+```bash
+git clone <paste copied URL here>
+```
+
+## To run this project:
+
+## Learning Objectives
+
---
+++ index.js
@@ -0,0 +1,77 @@
+/* global $, sessionStorage*/
+
+////////////////////////////////////////////////////////////////////////////////
+///////////////////////// INITIALIZATION ///////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+// Constant Variables
+
+
+// HTML jQuery Objects
+var $board = $('#board');
+var $scoreElement = $('#score');
+
+// game variables
+var score = 0;
+
+// interval variable required for stopping the update function when the game ends
+var updateInterval;
+
+// variable to keep track of the key (keycode) last pressed by the user
+var activeKey;
+var keysDown;
+
+// start the game
+startGame();
+
+////////////////////////////////////////////////////////////////////////////////
+///////////////////////// CORE LOGIC ///////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+function startGame() {
+  // empty the keysDown Object
+  keysDown = {};
+  
+  // turn on keyboard inputs
+  $('body').on('keydown', handleKeyDown);
+
+  // start the interval timer
+  updateInterval = setInterval(update, 100);
+}
+
+/* 
+ * Called once per "Interval"
+ */
+function update() {
+  
+}
+
+function endGame() {
+  // turn off keyboard
+  $('body').off();
+
+  // stop the interval timer
+  clearInterval(updateInterval);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+/* 
+  event.which returns the keycode of the key that is pressed when the
+  keydown event occurs
+  
+  The KEY Object creates a map for the Arrow Keys to their keycode:
+
+    KEY.LEFT = 37
+    KEY.UP = 38
+    KEY.RIGHT = 39
+    KEY.DOWN = 40
+  */
+function handleKeyDown(event) {
+  activeKey = event.which;
+  console.log(activeKey);
+
+  keysDown[activeKey] = true;
+}
