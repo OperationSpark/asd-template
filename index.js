929c8c1b5748db427146389d6bae2292e45c7e19
Ben Spector <30628696+benspector3@users.noreply.github.com>
2020-01-21 11:07:59 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,12 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 // Constant Variables
-
+var KEYS = {
+  "LEFT" : 37,
+  "UP": 38,
+  "RIGHT": 39,
+  "DOWN": 40
+};
 
 // HTML jQuery Objects
 var $board = $('#board');
