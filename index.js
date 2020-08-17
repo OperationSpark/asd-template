8b6ec9f863970c0e6ecff84bee23a6360429ca5e
Ben Spector <30628696+benspector3@users.noreply.github.com>
2020-08-17 10:48:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function runProgram(){
 
 
   // one-time setup
-  var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame 60 times per second.
+  var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
   $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
 
   ////////////////////////////////////////////////////////////////////////////////
